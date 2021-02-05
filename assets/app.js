286912d08db699d0f60d73765e49f6645d703e15
camaleoncoce <42813694+camaleoncoce@users.noreply.github.com>
2021-02-05 12:25:16 -0600

    linte time

--- assets/app.js
+++ assets/app.js
@@ -12,3 +12,33 @@ function mostrarScroll() {
 }
 
 window.addEventListener('scroll', mostrarScroll);
+
+/*==start modal==*/
+
+/*id in the modal */
+var modal = document.getElementById("modal__container");
+
+/*id in the card*/
+var card = document.getElementById("modal__event");
+
+/*==id btn close==*/
+var span = document.getElementsByClassName("close")[0];
+
+
+/*show modal*/
+card.onclick = function () {
+    modal.style.display = "block";
+}
+
+/*close modal*/
+span.onclick = function () {
+    modal.style.display = "none"
+}
+
+/*close modal onclick in the windows*/
+window.onclick = function (event) {
+    if (event.target === modal) {
+        modal.style.display = "none"
+    }
+}
+
